663bec243ec604ace2ef57d4c168f3a0eae67cdf
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2024-04-09 15:07:45 +0200

    feat:Connected to Mongodb.com's cloud server

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ var cors = require('cors');
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
-  if (error) {
-    console.error('Error connecting to MongoDB:', error);
-  } else {
-    console.log('Connected to MongoDB');
-  }
-});
-
+mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
