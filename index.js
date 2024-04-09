8c0d5cf9ce8110b8f2e607d382d564ef704149e1
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2024-04-09 21:31:41 +0200

    feat:Completed project

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 const mongoose = require('mongoose');
+var app = express();
+var multer = require('multer');// Middleware for handling multipart form data
+const upload = multer({ dest: 'uploads/' }); // Specify the directory where uploaded 
 
 mongoose.connect(process.env.MONGO_URI, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-var app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -18,7 +21,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // File has been uploaded and stored in the specified directory
+  // Access the uploaded file information using req.file object
+  const uploadedFile = req.file;
+  if (!uploadedFile) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: uploadedFile.originalname,
+    type: uploadedFile.mimetype,
+    size: uploadedFile.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
